0eea1a662743f2e479b3ebd27fb6ddfdb1926ee6
Juan Esteban Gil <juanestebang9@gmail.com>
2021-07-20 20:51:01 -0500

    Configured EF DataContext and ConnectionString

---
+++ PagosWebApi/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PagosWebApi.Models;
+
+namespace PagosWebApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
